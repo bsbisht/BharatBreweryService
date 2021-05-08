790f85e7416d4df1ee6c149e7db038eb92b49d6d
Bharat Bisht <bharatbisht071139ece@gmail.com>
2021-05-08 10:57:18 +0530

    Initial commit

---
+++ src/main/java/com/example/bharat/brewery/BharatBreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bharat.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatBreweryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatBreweryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bharat/brewery/BharatBreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bharat.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatBreweryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
